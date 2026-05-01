f30b5884f3e27e5a1b017ec3c67403689d2f41ae
rsibel11-png <rsibel11@gmail.com>
2026-04-30 23:03:34 -0700

    revert: remove broken diagnostic log (j not defined crash)

--- chartreview-aws/src/handlers/generate_summary.js
+++ chartreview-aws/src/handlers/generate_summary.js
@@ -632,9 +632,7 @@ const generateSummaryWorker = async (event) => {
         if (!result) continue;
         if (!patientName && result.patient_name) patientName = result.patient_name;
         if (!caseNumber && result.case_number) caseNumber = result.case_number;
-        console.log('Batch ' + (i+j+1) + ' result: visits=' + (result && Array.isArray(result.visits) ? result.visits.length : typeof result) + ' patient=' + (result ? result.patient_name : 'null'));
         const clean = sanitizeVisits(result.visits, patientName);
-        console.log('Batch ' + (i+j+1) + ' after sanitize: ' + clean.length + ' visits');
         allVisits = allVisits.concat(clean);
       }
     }
